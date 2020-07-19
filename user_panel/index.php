9e3021657917de3b8cec41ebf35239a72ad29b73
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-07-19 11:30:13 +0600

    Update index.php

--- user_panel/index.php
+++ user_panel/index.php
@@ -1,22 +1,39 @@
 <?php
 	session_start();
+	$id = 0;
 	
 	if(isset($_GET['logout'])){
 		session_destroy();
 		unset($_GET['userName']);
 	}
+	
+	elseif(isset($_GET['id'])){
+		$id = 1; //echo 'no';
+		//echo '<script type="text/javascript">','error();','</script>';
+	}
+	
 ?>
 
 
 <html>
 	<head>
 		<title>Index Page</title>
+		<script src="javascript.js"></script>
 	</head>
 	
 	<body>
 		<?php include('template/header.php') ?>
 		<?php include('template/footer.php') ?>
-		
-		
+		<!-- <script> alert('')</script> -->
+		<?php
+			if($id==1){
+				echo '<script type="text/javascript">','error("Access denied to home page");','</script>';
+				//$id = 1;
+			}
+			else{
+				header('location:home.php');
+			}
+			
+		?>
 	</body>
 </html>
