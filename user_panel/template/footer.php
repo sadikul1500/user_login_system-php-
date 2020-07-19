756ae631300e520dd21e28b4baa87537f2ea28e1
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-07-20 01:25:15 +0600

    Update footer.php

--- user_panel/template/footer.php
+++ user_panel/template/footer.php
@@ -1,5 +1,27 @@
-<body>
-	<footer class="section">
-		<div class="center grey-text"> Copyright 2020 @bsse1003</div>
-	</footer>
-</body>
+<html>
+
+	<head>
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<style type="text/css">
+			.foot {
+				position: fixed;
+				left: 0;
+				bottom: 0;
+				width: 100%;
+				background-color: #f8f9fa;
+				color: black;
+				text-align: center;
+			}
+		</style>
+	</head>
+
+
+	<body>
+		
+		<div class="foot">
+		<p>Copyright 2020 @bsse1003</p>
+		</div>
+		
+	</body>
+
+</html>
