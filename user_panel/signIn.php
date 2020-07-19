f82da199c253e4b503d378706e4f25f5197f4587
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-07-20 01:30:22 +0600

    Update signIn.php

--- user_panel/signIn.php
+++ user_panel/signIn.php
@@ -45,7 +45,7 @@
 <html>
 	<head>
 		<title>Sign In</title>
-		
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	</head>
 	<body>
 		<?php include('template/signIn_signUp.php'); ?>
